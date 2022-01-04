3b6b6cd118e79c44d765f95368ecb537027575eb
Landen Love <landenoooo@gmail.com>
2022-01-04 10:26:34 -0600

    Initial commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Survey</title>
+</head>
+<body>
+    <h1>Wel come to our web form</h1>
+    <p>Please enter your information below</p>
+    
+    <label for="firstname">First Name:</label>
+    <input type="text" id="firstname" name="firstname">
+    <label for="lastname">Last Name:</label>
+    <input type="text" id="lastname" name="lastname">
+
+</body>
+</html>
